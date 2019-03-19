9bebef952a0a858b828bcd16c2296a809e3dea93
Scoopta <scoopta@scoopta.ninja>
2019-03-19 13:25:45 -0700

    Fixed include path for arch

--- Release/plugins/subdir.mk
+++ Release/plugins/subdir.mk
@@ -4,12 +4,15 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../plugins/bind_mode.c \
 ../plugins/text.c 
 
 OBJS += \
+./plugins/bind_mode.o \
 ./plugins/text.o 
 
 C_DEPS += \
+./plugins/bind_mode.d \
 ./plugins/text.d 
 
 
@@ -17,7 +20,7 @@ C_DEPS += \
 plugins/%.o: ../plugins/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I../inc -I/usr/include/cairo -I/usr/include/atk-1.0 -I/usr/include/gtk-3.0 -I/usr/include/glib-2.0 -I/usr/include/pango-1.0 -I/usr/include/gdk-pixbuf-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -O3 -Wall -Wextra -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -I../inc -I/usr/include/cairo -I/usr/include/atk-1.0 -I/usr/include/gtk-3.0 -I/usr/include/glib-2.0 -I/usr/include/pango-1.0 -I/usr/lib/glib-2.0/include -I/usr/include/gdk-pixbuf-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -O3 -Wall -Wextra -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
