df6a2990e69fc6f66ce133643caf88a8ab0bfee6
Scoopta <scoopta@scoopta.ninja>
2019-03-19 19:44:00 -0700

    Added clock plugin

--- Debug/plugins/subdir.mk
+++ Debug/plugins/subdir.mk
@@ -5,16 +5,19 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../plugins/bind_mode.c \
+../plugins/clock.c \
 ../plugins/text.c \
 ../plugins/window.c 
 
 OBJS += \
 ./plugins/bind_mode.o \
+./plugins/clock.o \
 ./plugins/text.o \
 ./plugins/window.o 
 
 C_DEPS += \
 ./plugins/bind_mode.d \
+./plugins/clock.d \
 ./plugins/text.d \
 ./plugins/window.d 
 
