b026aa4f5bff92cb50e91aa102bf6ea04124df79
Scoopta <scoopta@scoopta.ninja>
2019-03-20 00:13:44 -0700

    Forgot to update the release makefile

--- Release/plugins/subdir.mk
+++ Release/plugins/subdir.mk
@@ -5,18 +5,27 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../plugins/bind_mode.c \
+../plugins/clock.c \
+../plugins/cpu.c \
 ../plugins/text.c \
-../plugins/window.c 
+../plugins/window.c \
+../plugins/workspace.c 
 
 OBJS += \
 ./plugins/bind_mode.o \
+./plugins/clock.o \
+./plugins/cpu.o \
 ./plugins/text.o \
-./plugins/window.o 
+./plugins/window.o \
+./plugins/workspace.o 
 
 C_DEPS += \
 ./plugins/bind_mode.d \
+./plugins/clock.d \
+./plugins/cpu.d \
 ./plugins/text.d \
-./plugins/window.d 
+./plugins/window.d \
+./plugins/workspace.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
